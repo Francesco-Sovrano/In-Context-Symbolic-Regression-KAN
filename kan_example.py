f084e747d583ff5cbf38d0c4d61b12d1552440bd
francesco.sovrano <38495561+Francesco-Sovrano@users.noreply.github.com>
2025-12-06 02:41:57 +0100

    Update kan_example.py

--- kan_example.py
+++ kan_example.py
@@ -13,42 +13,6 @@ def get_args():
 	)
 	return p.parse_args()
 
-args = get_args()
-print(args)
-SIMPLIFY = args.simplify
-
-# if torch.backends.mps.is_available():
-# 	device = torch.device("mps")        # Apple GPU via Metal
-# elif torch.cuda.is_available():
-# 	device = torch.device("cuda")       # For rare NVIDIA-eGPU setups
-# else:
-# 	device = torch.device("cpu")
-# print(device)
-
-# create a KAN: 2D inputs, 1D output, and 5 hidden neurons. cubic spline (k=3), 5 grid intervals (grid=5).
-# create dataset f(x,y) = exp(sin(pi*x)+y^2)
-f = lambda x: torch.exp(torch.sin(torch.pi*x[:,[0]]) + x[:,[1]]**2)
-f_range = [-1,1]
-dataset = create_dataset(f, ranges=f_range, n_var=2, train_num=2000, test_num=1000)
-print(dataset['train_input'].shape, dataset['train_label'].shape)
-
-lib = ['0', 'x','x^2','x^3','x^4','exp','log','sqrt','tanh','sin','abs']
-
-# plot KAN at initialization
-model = KAN(
-	width=[2, 50, 1], 
-	grid=50, 
-	grid_range=f_range,
-	atom_names=lib,
-	seed=0,
-	# numeric_atom_configs={
-	# 	"stepbf": {"num_grids": 20, "steepness": 10.},
-	# 	"rbf": {"num_grids": 50},
-	# },
-)
-# model(dataset['train_input']);
-# model.plot(beta=100)
-
 def check_gates(model):
 	"""
 	For every GatedSymbolicLayer:
@@ -104,16 +68,47 @@ def check_gates(model):
 				c_active = int(((best == k_idx) & active).sum().item())
 				print(f"{name:>8}  {mp:10.6f}  {c_all:13d}  {c_active:15d}")
 
-gate_top_k = 6
-for i in range(5):
-	# train the model
-	model.fit(dataset, opt="Adam", lr=1e-2, steps=500, lamb=1e-2);
-	model = model.prune(node_th=0.1, edge_th=0, gate_top_k=max(gate_top_k-i,3))
-	# check_gates(model)
-# model.prune_symbolic_gates_topk(k=3)
-model.fit(dataset, opt="Adam", lr=1e-2, steps=500)
+args = get_args()
+print(args)
+SIMPLIFY = args.simplify
 
-print('model.get_symbolic_choice_per_edge:', model.get_symbolic_choice_per_edge())
+# if torch.backends.mps.is_available():
+# 	device = torch.device("mps")        # Apple GPU via Metal
+# elif torch.cuda.is_available():
+# 	device = torch.device("cuda")       # For rare NVIDIA-eGPU setups
+# else:
+# 	device = torch.device("cpu")
+# print(device)
+
+# create a KAN: 2D inputs, 1D output, and 5 hidden neurons. cubic spline (k=3), 5 grid intervals (grid=5).
+# create dataset f(x,y) = exp(sin(pi*x)+y^2)
+f = lambda x: torch.exp(torch.sin(torch.pi*x[:,[0]]) + x[:,[1]]**2)
+f_range = [-1,1]
+dataset = create_dataset(f, ranges=f_range, n_var=2, train_num=2000, test_num=1000)
+print(dataset['train_input'].shape, dataset['train_label'].shape)
+
+lib = ['0', 'x','x^2','x^3','x^4','exp','log','sqrt','tanh','sin','abs']
+
+# plot KAN at initialization
+model = KAN(
+	width=[2, [5,5], 1], 
+	grid=50, 
+	grid_range=f_range,
+	seed=0,
+	# atom_names=lib,
+	# numeric_atom_configs={
+	# 	"stepbf": {"num_grids": 20, "steepness": 10.},
+	# 	"rbf": {"num_grids": 50},
+	# },
+)
+
+model.fit(dataset, opt="Adam", lr=1e-2, steps=1000, lamb=1e-3)
+for i in range(5):
+	model.mult_eps = min(1, (1/4)*i)
+	model = model.prune(node_th=0.1, edge_th=0)
+	model.fit(dataset, opt="Adam", lr=1e-2, steps=1000, lamb=1e-3)
+	# check_gates(model)
+	print(model.get_symbolic_choice_per_edge())
 
 mode = "auto" # "manual"
 if mode == "manual":
@@ -123,24 +118,24 @@ if mode == "manual":
 	model.fix_symbolic(1,0,0,'exp');
 elif mode == "auto":
 	# model.auto_symbolic(lib=lib, weight_simple=0)
-	model, summary = model.auto_symbolic_robust_greedy(
+	summary = model.auto_symbolic_robust_greedy(
 		dataset,       # evaluation set
 		lib=lib,
-		min_edge_score=None,         # or e.g. 1e-3 to stop earlier
-		weight_simple=0,
+		min_edge_score=0.1,
+		# weight_simple=0,
 		# verbose=1,
 		lr=1e-2,
 		steps=100,
-		# lamb=1e-3,
-		# node_th=0.01, edge_th=0,
+		# lamb=1e-2,
+		# node_th=0.1, edge_th=0.1,
 		top_k_gates=3
 	)
 	print('auto_symbolic_robust_greedy:', summary)
 
-model.fit(dataset, opt="Adam", lr=1e-2, steps=500)
+model.fit(dataset, opt="Adam", lr=1e-3, steps=1000)
 # model.fit(dataset, opt="LBFGS", lr=1e-1, steps=500, gating_entropy=1e-1)
-print('model.get_symbolic_choice_per_edge:', model.get_symbolic_choice_per_edge())
-check_gates(model)
+# print(model.get_symbolic_choice_per_edge())
+# check_gates(model)
 
 symbolic_formula = model.symbolic_formula(simplify=SIMPLIFY)
 if symbolic_formula:
